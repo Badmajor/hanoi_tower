3cde8e6d2cfeaa3281b1cffdcf282eedc02be536
Badmajor <vtk88@mail.ru>
2024-02-05 12:53:54 +0300

    new version

--- classes.py
+++ classes.py
@@ -11,27 +11,34 @@ class Block:
     def __repr__(self):
         return self.__str__()
 
+    def __lt__(self, other):
+        if isinstance(other, type(self)):
+            return self.name < other.name
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, type(self)):
+            return self.name > other.name
+        return NotImplemented
+
 
 class Column:
     def __init__(self, amount_blocks: int, first_block=False):
-        self.empty_block = Block(0)
-        self.blocks = [self.empty_block] * amount_blocks
+        self.blocks = []
         self.amount_blocks = amount_blocks
         if first_block:
             self.__add_starts_block()
 
     def add_block(self, obj: Block):
-        if self.is_empty() or self.upper_block.name > obj.name:
-            index = self.count_blocks()
-            self.blocks[index] = obj
-        else:
-            raise UnsuitableBlock(f'Column: {self} -> Block: {obj}')
+        if not self.is_empty() and self.blocks[-1] < obj:
+            raise UnsuitableBlock
+        self.blocks.append(obj)
 
     def is_empty(self):
-        return not bool(self.blocks[0].name)
+        return not bool(self.blocks)
 
     def _is_full(self):
-        return bool(self.blocks[-1].name)
+        return bool(len(self.blocks) == self.amount_blocks)
 
     @property
     def upper_block(self):
@@ -43,33 +50,19 @@ class Column:
     def retrieve_block(self):
         if self.is_empty():
             raise EmptyColumn
-        block = self.blocks.pop(self.count_blocks() - 1)
-        self.blocks.append(self.empty_block)
+        block = self.blocks.pop()
         return block
 
     def __add_starts_block(self):
         """adding starts blocks"""
         for i in range(self.amount_blocks, 0, -1):
-            self.blocks[self.amount_blocks - i] = Block(i)
+            self.blocks.append(Block(i))
 
     def __str__(self):
-        return f'{self.blocks}'
+        return f'{self.blocks + [0 for _ in range(self.amount_blocks - self.count_blocks())]}'
 
     def count_blocks(self):
-        if self.is_empty():
-            return 0
-        if self._is_full():
-            return self.amount_blocks
-        low, high = 0, self.amount_blocks -1
-        while low <= high:
-            mid = (low + high) // 2
-            if self.blocks[mid].name:
-                if self.blocks[mid + 1].name:
-                    low = mid + 1
-                else:
-                    return mid + 1
-            else:
-                high = mid - 1
+        return len(self.blocks)
 
     def __repr__(self):
         return self.__str__()
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import time
 
 from classes import HanoiTower
